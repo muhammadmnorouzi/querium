685e1aad49584aa98776dd2d108849a7f0999eb9
Muhammad Mahdi Norouzi <muhammadmnorouzi@gmail.com>
2025-08-07 10:36:16 +0330

    introduced Quantiq presentation project.

---
+++ Arian.Quantiq/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Arian.Quantiq.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
